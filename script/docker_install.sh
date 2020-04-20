0ce69c13cc69297e14f545d4483350f214543546
bugybq <51486805+bugybq@users.noreply.github.com>
2020-04-20 13:23:14 +0800

    Update docker_install.sh

--- script/docker_install.sh
+++ script/docker_install.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 # install docker and docker-compose
 
+# ====== apt update first
 echo -e "\033[32m apt update \n \033[0m"
 sudo apt-get update 
 
+# ====== set timezone to Shanghai
 echo -e "\033[32m Set timezone to Asia/Shanghai \n \033[0m"
 sudo teimdatectl set-timezone Asia/Shanghai
 
+# ====== install htpasswd
 echo -e "\033[32m Install htpasswd to generate basic auth password \n \033[0m"
 # echo $(htpasswd -nb user password) | sed -e s/\\$/\\$\\$/g
 sudo apt install -y apache2-utils
 
-
-
+# ====== remove old docker installation
 echo -e "\033[32m remove old all docker installation \n \033[0m"
 sudo apt-get remove docker docker-engine docker.io containerd runc
 
-
+# ====== Install docker
 echo -e "\033[32m setup docker respository \n \033[0m"
 
 echo -e "\033[32m setup apt over https \n \033[0m"
@@ -43,7 +45,7 @@ sudo apt-get install -y docker-ce=5:19.03.8~3-0~ubuntu-bionic docker-ce-cli=5:19
 echo -e "\033[32m add ubuntu user to docker group \n \033[0m"
 sudo usermod -aG docker ubuntu
 
-
+# ====== Install docker-compose
 echo -e "\033[32m download and install docker compose \n \033[0m"
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
